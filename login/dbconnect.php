4869470b6c98d874f745a29c1d367641105b4c8f
Yaagesh <yaageshrp@gmail.com>
2018-03-16 12:25:25 +0530

    1st rep commit
    Start of rep

---
+++ login/dbconnect.php
@@ -0,0 +1,22 @@
+<?php
+
+	// this will avoid mysql_connect() deprecation error.
+	error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+	// but I strongly suggest you to use PDO or MySQLi.
+	
+	define('DBHOST', 'localhost');
+	define('DBUSER', 'root');
+	define('DBPASS', '');
+	define('DBNAME', 'dbtest');
+	
+	$conn = mysqli_connect(DBHOST,DBUSER,DBPASS);
+	$dbcon = mysqli_select_db($conn,DBNAME);
+	$mysqli=mysqli_connect('localhost','root','','dbtest');
+	
+	if ( !$conn ) {
+		die("Connection failed : " . mysqli_error());
+	}
+	
+	if ( !$dbcon ) {
+		die("Database Connection failed : " . mysqli_error());
+	}
